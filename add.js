5911f4326931d97f07dde3eda26002db4aa1d138
BManusha <109844101+BManusha@users.noreply.github.com>
2022-09-08 08:48:07 +0530

    Update add.js
    103

--- add.js
+++ add.js
@@ -4,5 +4,4 @@ var num1 = document.getElementById("firstno").value
 var num2 = document.getElementById("firstno").value
 var result = num1+num2
 document.getElementById("result").value = result
-
 }
